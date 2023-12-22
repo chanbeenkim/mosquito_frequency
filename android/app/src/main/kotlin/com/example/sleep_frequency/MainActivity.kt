f2c65849841a524b52a34e7de5f0283f84a48f1c
beenfilm94@gmail.com <kcb9582@naver.com>
2023-12-22 15:58:29 +0900

    모기퇴치 주파수 어플

---
+++ android/app/src/main/kotlin/com/example/sleep_frequency/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sleep_frequency
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:surround_sound/surround_sound.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final _controller = SoundController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: const Text(
+          "Mosquito frequency",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SoundWidget(
+            soundController: _controller,
+          ),
+          SizedBox(
+            height: 160,
+            width: 160,
+            child: Image.asset(
+              "assets/images/mosquito.png",
+            ),
+          ),
+          const SizedBox(
+            height: 60,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                iconSize: 80,
+                onPressed: () async {
+                  await _controller.play();
+                },
+                icon: Icon(
+                  Icons.play_circle_fill,
+                  color: Colors.red.shade700,
+                ),
+              ),
+              IconButton(
+                iconSize: 80,
+                onPressed: () async {
+                  await _controller.stop();
+                },
+                icon: const Icon(
+                  Icons.stop_circle,
+                  color: Colors.black,
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 18,
+          ),
+          ValueListenableBuilder(
+            valueListenable: _controller,
+            builder: (context, value, _) {
+              return Column(
+                children: [
+                  const Text(
+                    "Volume",
+                    style: TextStyle(fontWeight: FontWeight.w800),
+                  ),
+                  Slider(
+                    thumbColor: Colors.black,
+                    activeColor: Colors.red,
+                    inactiveColor: Colors.grey,
+                    value: value.volume,
+                    min: 0,
+                    max: 1,
+                    onChanged: (val) {
+                      _controller.setVolume(val);
+                    },
+                  ),
+                  const Text("Frequency",
+                      style: TextStyle(fontWeight: FontWeight.w800)),
+                  Slider(
+                    thumbColor: Colors.black,
+                    activeColor: Colors.red,
+                    inactiveColor: Colors.grey,
+                    value: value.freq,
+                    min: 128,
+                    max: 1500,
+                    onChanged: (val) {
+                      _controller.setFrequency(val);
+                    },
+                  ),
+                ],
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
